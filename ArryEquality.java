1519e191c5e42d4af94a921554ed7d967e77ec50
Lalit3610 <109783524+Lalit3610@users.noreply.github.com>
2022-08-09 13:55:48 +0530

    Add files via upload

---
+++ ArryEquality.java
@@ -0,0 +1,11 @@
+// to test the equality of two arrays
+import java.util.Arrays;
+class ArrayEquality
+{
+	public static void main(String args[])
+	{
+		int []a=new int[]{1,2,3,4,5};
+		int []b=new int[]{1,2,3,4,5};
+		System.out.println(Arrays.equals(a,b));
+	}
+}
